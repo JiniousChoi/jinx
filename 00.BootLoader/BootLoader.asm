8fd26977d875e197c22dd2a51668f1d2ca531694
jin choi <jinchoiseoul@gmail.com>
2018-01-22 21:17:29 +0900

    [00.BootLoader/BootLoader.asm] introduce loop for bootloader message

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -3,19 +3,39 @@
 
 SECTION .text
 
-mov ax, 0xB800
-mov ds, ax
+jmp 0x07C0:START                    ; assign code segment and then jump to the label
 
-mov byte [0x00], 'J'
-mov byte [0x01], 0x42
-mov byte [0x02], 'I'
-mov byte [0x03], 0x42
-mov byte [0x04], 'N'
-mov byte [0x05], 0x42
-mov byte [0x06], 'X'
-mov byte [0x07], 0x42
+START:
+    mov ax, 0x07C0
+    mov ds, ax                      ; data segment for memory access
+    mov ax, 0xB800
+    mov es, ax                      ; es mostly used for string manipulation
+    mov si, 0
 
-jmp $
+.SCREENCLEARLOOP:   ;do while
+    mov byte [es:si], 0
+    mov byte [es:si+1], 0x0A
+    add si, 2
+    cmp si, 80 * 25 * 2
+    jl .SCREENCLEARLOOP
+
+    mov si, 0                       ; si for source idx of string
+    mov di, 0                       ; di for destination idx of string
+
+.MESSAGELOOP:
+    mov cl, byte [si + MESSAGE1]    ; same as [ds: si + MESSAGE1]
+    cmp cl, 0
+    je .MESSAGEEND
+
+    mov byte [es:di], cl            ; memory(si) -> register(cl) -> memory(di)
+    add si, 1
+    add di, 2
+    jmp .MESSAGELOOP
+
+.MESSAGEEND:
+    jmp $
+
+MESSAGE1: db 'JINX OS Bootloader Start ...', 0
 
 times 510-($-$$) db 0x00
 
